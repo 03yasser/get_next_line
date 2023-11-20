c6b0121abd29299d4712b0e3aad53a1bd019dac4
03yasser <yasserboutslighoi03@gmail.com>
2023-11-20 13:39:40 +0100

    done

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: yboutsli <yboutsli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 10:51:19 by yboutsli          #+#    #+#             */
-/*   Updated: 2023/11/19 21:05:44 by yboutsli         ###   ########.fr       */
+/*   Updated: 2023/11/20 13:38:23 by yboutsli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,11 @@ char *fill_line_buffer(int fd, char *left_l, char *buffer)
 		bytes = read(fd, buffer, BUFFER_SIZE);
 		// printf ("bytes : %zd\n", bytes);
 		if (bytes == -1)
+		{
+			free(left_l);
+			left_l = NULL;
 			return (NULL);
+		}
 		else if (bytes == 0)
 			break;
 		buffer[bytes] = '\0';
@@ -38,6 +42,12 @@ char *fill_line_buffer(int fd, char *left_l, char *buffer)
 		tmp = left_l;
 		left_l = ft_strjoin(tmp, buffer);
 		//printf ("line : [%s]\n",left_l);
+		if (tmp == left_l)
+		{
+			free (tmp);
+			tmp = NULL;
+			return (NULL);
+		}
 		free (tmp);
 		tmp = NULL;
 		if (nl_exist(left_l))
@@ -62,6 +72,8 @@ char	*set_left(char *line)
 		return (NULL);
 	left_l = ft_substr(line, i + 1, ft_strlen(line) - i - 1);
 	line[i + 1] = '\0';
+	if (i == 0)
+		line = NULL;
 	//printf("[%s]", line);
 	if (!left_l)
 		return (line);
@@ -87,8 +99,12 @@ char *get_next_line(int fd)
 	line = fill_line_buffer(fd, left_l, buffer);
 	//printf("[%s]\n", line);
 	free (buffer);
-	if (!line)
+	if (!line || line[0] == '\0')
+	{
+		free (left_l);
+		left_l = NULL;
 		return (NULL);
+	}
 	left_l = set_left(line);
 	//printf("[%s]\n", left_l);
 	return (line);
@@ -97,12 +113,12 @@ char *get_next_line(int fd)
 // int main()
 // {
 // 	char *str;
-// 	int fd = open ("files/variable_nls.txt", O_RDONLY);
+// 	int fd = open ("files/lines_around_10.txt", O_RDONLY);
 // 	for (int i = 0; i < 8; i++)
 // 	{
 // 		//printf (":%d:\n", i);
 // 		str = get_next_line(fd);
-// 		//printf("[%s]\n", str);
+// 		printf("[%s] ", str);
 // 		free (str);
 // 	}
 // 	// char buffer[BUFFER_SIZE];
